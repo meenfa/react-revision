7b7afec9967e0177b71a56a42419afce90a854c3
ankitkarki27 <ankitkarki8088@gmail.com>
2026-07-01 14:40:55 +0545

    localStorage  to store user data done

--- 10-localstorage/src/App.jsx
+++ 10-localstorage/src/App.jsx
@@ -1,12 +1,26 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  // localStorage.setItem("user", "Ankit");
 
-  return <div>this is for local storage part</div>;
+  // const user = localStorage.getItem("user");
+  // console.log("value is", user);
+
+  // localStorage.removeItem("user");
+  // localStorage.clear();
+
+  const user = {
+    username: "ankit",
+    age: 20,
+    city: "kathmandu",
+  };
+
+  localStorage.setItem("user", JSON.stringify(user));
+
+  console.log(user);
+
+  return <div>Open the Console to See the Output</div>;
 }
 
 export default App;
